12b17d911614b2a8fad6cd1eadf3c04033588ce0
oasis636 <125544598+oasis636@users.noreply.github.com>
2024-03-15 14:47:21 +0000

    Everything positive is good

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
-#!/usr/bin/python3
-for ch in reversed(range(97, 123)):
-    print("{:c}".format(ch if (ch % 2 == 0) else (ch - 32)), end='')
+#!/usr/bin/python3 
+for i in range(122, 96, -1):
+    print("{:s}".format(chr(i) if i % 2 == 0 else chr(i - 32)), end="")
--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,52 +1,40 @@
 #include "lists.h"
-#include <stdlib.h>
-#include <stddef.h>
-
-/**
- * insert_node -  inserts a number into a sorted singly linked list
- * @head: double pointer to head node of the linked list
- * @number:  number to  insert
- * Return: the address of the new node
- *         else NULL if it failed
-*/
 
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *tmp, *n_node, *old = NULL;
+	listint_t *new_node, *current_node;
+	if (head == NULL)
+		return NULL;
 
-	if (!head)
+	new_node = malloc(sizeof(listint_t));
+
+	if (new_node == NULL)
 		return (NULL);
-
-	n_node = malloc(sizeof(listint_t));
-	if (!n_node)
-		return (NULL);
-
-	n_node->n = number;
-	n_node->next = NULL;
-
-	tmp = *head;
-	while (tmp && n_node->n > tmp->n)
+	current_node = *head;
+	new_node->n = number;
+	new_node->next = NULL;
+	if (*head == NULL)
 	{
-		old = tmp;
-		tmp = tmp->next;
+		*head = new_node;
+		return (new_node);
 	}
-	/* for double node */
-	if (tmp && n_node->n == tmp->n)
+	if (current_node->n > number)
 	{
-		n_node->next = tmp;
-		old->next = n_node;
+		new_node->next = current_node;
+		*head = new_node;
+		return (new_node);
 	}
-	/* if n_node  is low than first node */
-	else if (!old)
+	
+	while (current_node->next != NULL)
 	{
-		n_node->next = *head;
-		*head = n_node;
+		if(current_node->next->n > number)
+		{
+			new_node->next = current_node->next;
+			current_node->next = new_node;
+			return new_node;
+		}
+		current_node = current_node->next;
 	}
-	else
-	{
-		n_node->next = old->next;
-		old->next = n_node;
-	}
-
-	return (n_node);
+	current_node->next = new_node;
+	return (new_node);
 }
