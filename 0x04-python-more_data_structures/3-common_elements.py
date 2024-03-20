85a29157ea730e8ca8d468f1192e56d9feab0691
Oasis808 <isaiahadiamah20@gmail.com>
2024-03-20 15:41:12 -0700

    Search and replace

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# -----------------------------------------------------------
+# Python program that:
+# demonstrates how to return a set of common elements in two sets
+#
+# (C) 2024 Adiamah Isaiah, Kumasi, Ghana
+# email isaiahadiamah20@gmail.com
+# -----------------------------------------------------------
+
+
+def common_elements(set_1, set_2):
+    return set_1 & set_2
