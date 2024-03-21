73b34efd2c75ddb251d21f4187d2e2805e22c959
Oasis808 <isaiahadiamah20@gmail.com>
2024-03-20 19:58:59 -0700

    Roman to integer

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -8,6 +8,5 @@
 # email isaiahadiamah20@gmail.com
 # -----------------------------------------------------------
 
-
 def square_matrix_map(matrix=[]):
-    return list(map(lambda x: list(map(lambda i: i ** 2, x)), matrix))
+    return list(map(lambda row: list(map(lambda i: i ** 2, row)), matrix))
--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -8,6 +8,5 @@
 # email isaiahadiamah20@gmail.com
 # -----------------------------------------------------------
 
-
 def multiply_list_map(my_list=[], number=0):
-    return list(map(lambda i: number * i, my_list))
+    return list(map(lambda i: i * number, my_list))
